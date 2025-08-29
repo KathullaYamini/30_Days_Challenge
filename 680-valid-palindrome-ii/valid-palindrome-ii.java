4e74dfd3f886566f0f0a18dbe869d939c8ae7d2a
KathullaYamini <156216974+KathullaYamini@users.noreply.github.com>
2025-08-29 22:32:24 +0530

    Time: 4 ms (99.61%) | Memory: 46.1 MB (5.05%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrom(String s,int i,int j){
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return isPalindrom(s,i+1,j) || isPalindrom(s,i,j-1);
+            }
+            i++;
+            j--;
+            
+        }
+        
+        return true;
+        
+    }
+}
