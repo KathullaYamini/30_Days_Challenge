8d02dbaa16c4c37adb7a7718c9d7f4b65699d883
KathullaYamini <156216974+KathullaYamini@users.noreply.github.com>
2025-09-04 19:05:05 +0530

    Time: 2 ms (93.37%) | Memory: 56 MB (53.86%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+       int f=0;
+       int t=0;
+       for(int i=0;i<bills.length;i++){
+        if(bills[i]==5){
+            f++;
+        }
+        else if(bills[i]==10){
+            if(f==0){
+                return false;
+            }
+            f--;
+            t++;
+            
+        }
+        else{
+            if(t>0 && f>0){
+                t--;
+                f--;
+            }
+            else if(f>2){
+                f-=3;
+            }
+            else{
+                return false;
+            }
+        }
+       }
+       return true;
+    }
+}
