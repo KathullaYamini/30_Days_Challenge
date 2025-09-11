2b8661a3a8984b0fd8b0460fbc964437d0547dfd
KathullaYamini <156216974+KathullaYamini@users.noreply.github.com>
2025-09-11 20:54:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (32.24%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -4,12 +4,8 @@ class Solution {
         for(int i=1;i<strs.length;i++){
             while(!strs[i].startsWith(res)){
                 res=res.substring(0,res.length()-1);
-                if(res.isEmpty()){
-                    return "";
-                }
             }
         }
         return res;
-        
     }
 }
