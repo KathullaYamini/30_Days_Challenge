445e7e6d953e6ceb407dc25e64bb65c675c77def
KathullaYamini <156216974+KathullaYamini@users.noreply.github.com>
2025-09-19 21:53:42 +0530

    Time: 122 ms (50.35%) | Memory: 55.8 MB (87.32%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.java
@@ -0,0 +1,44 @@
+class Spreadsheet {
+    private int[][] grid; 
+    
+    public Spreadsheet(int rows) {
+        grid = new int[rows][26]; 
+    }
+    
+    public void setCell(String cell, int value) {
+        int[] pos = parseCell(cell);
+        grid[pos[0]][pos[1]] = value;
+    }
+    
+    public void resetCell(String cell) {
+        int[] pos = parseCell(cell);
+        grid[pos[0]][pos[1]] = 0;
+    }
+    
+    public int getValue(String formula) {
+        
+        formula = formula.substring(1);
+        String[] parts = formula.split("\\+");
+        
+        int val1 = getOperandValue(parts[0]);
+        int val2 = getOperandValue(parts[1]);
+        
+        return val1 + val2;
+    }
+    
+    private int[] parseCell(String cell) {
+        char colChar = cell.charAt(0);
+        int col = colChar - 'A';
+        int row = Integer.parseInt(cell.substring(1)) - 1;
+        return new int[]{row, col};
+    }
+    
+    private int getOperandValue(String operand) {
+        if (Character.isDigit(operand.charAt(0))) {
+            return Integer.parseInt(operand);
+        } else {
+            int[] pos = parseCell(operand);
+            return grid[pos[0]][pos[1]];
+        }
+    }
+}
